5ec134b376b4fc99493d7cf457895d33bc64d3b2
Jonna <jonnabr@hotmail.com>
2021-04-26 15:32:58 +0200

    Adding 2 functions: 'get_dbpedia_resource' and 'connect_dbpedia_resource'

--- main.py
+++ main.py
@@ -11,26 +11,6 @@ from get_liquipedia_data import team_results, player_results, map_results
 # Note: importing team_results, and player_results may takes a bit of time.
 #       This is due to the API terms of use described in get_liquipedia_data.py
 
-# static variable
-SERVER = "https://api.dbpedia-spotlight.org/en/annotate"
-
-# Instantiate graph
-g = Graph()
-
-# Namespaces
-ex = Namespace("http://example.org/")
-dbp = Namespace("http://dbpedia.org/resource/")
-schema = Namespace("https://schema.org/")
-dbp_o = Namespace('http://dbpedia.org/ontology/')
-
-# bind namespaces
-g.bind('FOAF', FOAF)
-g.bind("ex", ex)
-g.bind("dbp", dbp)
-g.bind("Schema", schema)
-g.bind("DBpedia", dbp_o)
-
-
 # import datasets to one big df
 all_dfs = []
 phs_dir = os.getcwd() + r'\phs_data'
@@ -49,47 +29,91 @@ for sub_dir in os.listdir(phs_dir):
 
 dfs = pd.concat(all_dfs)
 
-# list of already queried DBpedia resources
+# Instantiate graph
+g = Graph()
+
+# Namespaces
+ex = Namespace("http://example.org/")
+dbp = Namespace("http://dbpedia.org/resource/")
+schema = Namespace("https://schema.org/")
+dbp_o = Namespace('http://dbpedia.org/ontology/')
+
+# bind namespaces
+g.bind('FOAF', FOAF)
+g.bind("ex", ex)
+g.bind("dbp", dbp)
+g.bind("Schema", schema)
+g.bind("DBpedia", dbp_o)
+
+SERVER = "https://api.dbpedia-spotlight.org/en/annotate"
+
+# global list of already queried DBpedia resources
 queried_resources = set()
+all_resources = {}
+
+
+def get_dbpedia_resources(resources):
+    '''
+    Requests DBpedia's API with the spotlight module to gather data about resources
+    :param resources: list of resources to query (cannot contain list of lists)
+    :return list of dicts: response from DBpedia's API
+    '''
+    global queried_resources
+    server = "https://api.dbpedia-spotlight.org/en/annotate"
+    # make sure resource is not already queried, to avoid unnecessary API requests
+    resources = [str(x) for x in resources if x not in queried_resources]
+    # verify that we still have some resources after filtering out already queried ones
+    if resources == []:
+        print('All resources were already queried')
+        return None
+    # add resources to queried_resources
+    queried_resources.update(resources)
+    # convert list of resources to comma-separated string for API request
+    text = ','.join(resources)
+    # perform API request
+    try:
+        response = spotlight.annotate(server, text)
+        # add this response to the global variable all_resources
+        [all_resources.update({x['surfaceForm']: response}) for x in response]
+        return response
+    except Exception as e:
+        print('REEEE', e)
+
+
+def connect_dbpedia_resources(response, team_data, types):
+    '''
+    Connect the resources gathered from DBpedia's API with the resources from our dataset.
+    '''
+    if response is None:
+        return None
+    # check if the queried resource string == the value of the resource in team_data
+    result = {t: r for r in response for t in types if r['surfaceForm'] == team_data[t]}
+    # MANUALLY add exceptions like 'Has sponsor'
+    if 'Has sponsor' in types:
+        sponsors = [x[0] for x in team_data['Has sponsor']]
+        sponsors = [r for r in response if r['surfaceForm'] in sponsors]
+        if sponsors != []:
+            sponsor_dict = {'Has sponsor': sponsors}
+            result.update(sponsor_dict)
+    return result
+
 
 for team, team_data in team_results.items():
     # try to get resource from DBpedia first
-    whitelist = ['Has name', 'Has region', 'Has location', 'Has sponsor']
-    text = [team_data[x][0][0] if x == 'Has sponsor' else team_data[x] for x in whitelist if x in team_data.keys()]
-    text = ','.join(text)
-    #
-    annotations = spotlight.annotate(SERVER, text)
-    #
+    whitelist = ['Has name', 'Has region', 'Has location']
+    # add all resources to one list for later query
+    try:
+        sponsors = [x[0] for x in team_data['Has sponsor']]
+    except KeyError:
+        sponsors = []
+    resources = [team_data[x] for x in whitelist if x in team_data.keys()] + sponsors
+    # query resources
+    response = get_dbpedia_resources(resources)
+    # connect resources with team_data
+    resources = connect_dbpedia_resources(response, team_data, whitelist)
+    # 
     team = team.replace(' ', '_')
-    for annotation in annotations:
-        # see which of the team_data values it matched
-        if team_data.get('Has name'):
-            has_name = re.fullmatch(annotation['surfaceForm'], team_data['Has name'])
-        if team_data.get('Has region'):
-            has_region = re.fullmatch(annotation['surfaceForm'], team_data['Has region'])
-        if team_data.get('Has location'):
-            has_location = re.fullmatch(annotation['surfaceForm'], team_data['Has location'])
-        if team_data.get('Has sponsor'):
-            has_sponsor = re.fullmatch(annotation['surfaceForm'], team_data['Has sponsor'][0][0])
-        #
-        if has_name is not None:
-            # team_entity, FOAF.name, team
-            team_entity = URIRef(annotation['URI'])
-            g.add((team_entity, FOAF.name, Literal(team_data['Has name'])))
-        elif has_region is not None:
-            # team_entity, dbp_o:region, region
-            region = URIRef(annotation['URI'])
-            g.add((team_entity, dbp_o.term('region'), region))
-        elif has_location is not None:
-            # team_entity, dbp_o:location, location
-            location = URIRef(annotation['URI'])
-            g.add((team_entity, dbp_o.term('location'), location))
-        elif has_sponsor is not None:
-            # team_entity, dbp_o:sponsor, sponsor
-            sponsor = URIRef(annotation['URI'])
-            g.add((team_entity, dbp.term('sponsor'), sponsor))
-            # what to do with types
-    #
+
     ### DATASET STUFF (veldig temp) ###
     #df = dfs.loc[dfs['team'] == team]
     #df = df.drop_duplicates(subset=['start_time','match_id','stage','map_type','map_name','player','team'])
@@ -130,39 +154,6 @@ for row in zip(player_data['player'], player_data['team']):
         g.add((player_entity, dbp_o.term('country'), country_URIref))
     except KeyError:
         continue
-
-
-##### TEST STUFF
-
-
-# Add team triples to graph
-# for team, team_data in team_results.items():
-#     ### DBpedia ###
-#     # try to get resource from DBpedia first
-#     whitelist = ['Has name', 'Has region', 'Has location', 'Has sponsor']
-#     resources = [team_data[x][0][0] if x == 'Has sponsor' else team_data[x] for x in whitelist if x in team_data.keys()]
-    
-#     # if we already queried the resource, don't do it again
-#     queried_resources.update(resources)
-#     text = ','.join(resources)
-#     #
-#     annotations = spotlight.annotate(SERVER, text)
-#     #
-#     team = team.replace(' ', '_')
-#     for annotation in annotations:
-#         #
-#         for prop in whitelist:
-#             # if team_data[prop] exists
-#             if team_data.get(prop):
-#                 resource_name = re.fullmatch(annotation['surfaceForm'],team_data[prop])
-#                 # if we found a resource in DBpedia for this resource, and the name property name is 'Has name'
-#                 if resource_name is not None and prop == 'Has name':
-#                     team_entity = URIRef(annotation['URI'])
-#                     g.add((team_entity, FOAF.name, Literal(team_data['Has name'])))
-#                 # if we found a resource in DBpedia for this resource, do some stuff
-#                 elif resource_name is not None:
-#                     resource_uri = URIRef(annotation['URI'])
-#                     g.add((team_entity, dbp_o.term(prop), resource_uri))
 
 
 # Print the graph to terminal
